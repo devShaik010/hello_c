12eb3479ffff6c47ea9608305ec5403d23407a0c
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-12-02 09:52:15 -0800

    this is hello world program in c
    i already uploaded this c file but after i see its a blank that is reason i update this file .

--- hello.c
+++ hello.c
@@ -1,1 +1,12 @@
+/* hello world program in c */
+#include <stdio.h>
+#include <conio.h>
+
+/* start main function */
+void main()
+{
+  printf("hello world");
+  getch();
+  
+} /* end of the program */
 
