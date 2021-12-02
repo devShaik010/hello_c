3ac7a1d0b9948eb351da9eb91e2432bcf25b555a
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-12-02 09:54:58 -0800

    conditional operater in c language
    its is a program for conditional operator in c language ,,, lets see and run this program

---
+++ conditionalop.c
@@ -0,0 +1,15 @@
+/*conditional operator */
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int x,y,z;
+  clrscr;
+  x=10;
+  y=20;
+   /* using relational operator here */
+  z=(x>y)?x:y;
+  printf("the biggest number is %d",z);
+  getch();
+
+}
