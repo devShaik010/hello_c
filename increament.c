7830e028a8ceeef43e60b842397d26af8cb33994
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-12-02 10:00:28 -0800

    its a increament and decreament program
    its a increment and decrement program inc language , using this program u can decrease 1 number in the given variable value and also u can increase 1 using increment , for more more understanding u can simply run program and try to understand this code , if any mistake and bug in this code pleas comment ,me ....,,,,,,

---
+++ increament.c
@@ -0,0 +1,42 @@
+/* find increament and decreament program in c language */
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int num1,num2,num3,num4;
+    num1=20;
+    num2=30;
+    /* increament program */
+    num3=++num1;
+    printf("the increament number is %d",num3);
+
+    /* program for decreament */
+    num4=--num2;
+
+    printf("the decreament number is %d",num4);
+    getch();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
